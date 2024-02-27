6ef9958a9262042325ac579721f35c9dbd02e80c
qdlyh <yh620@foxmail.com>
2024-02-27 14:21:59 +0800

    添加文章分页以及模糊查询功能

--- src/article/article.controller.ts
+++ src/article/article.controller.ts
@@ -1,4 +1,4 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Query } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { CreateArticleDTO, EditArticleDTO } from './article.dto';
 import { Body, Delete, Get, Param, Post, Put } from '@nestjs/common';
@@ -6,19 +6,37 @@ import { Body, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('article')
 export class ArticleController {
-  constructor(private articleService: ArticleService) {}
+  constructor(private articleService: ArticleService) { }
 
   @Get('list')
-  async findAll(): Promise<object> {
-    return {
-      code: 200,
-      data: await this.articleService.findAll(),
-      message: 'Success',
-    };
+  //http://localhost:3000/app/article/list
+  // async findAll(): Promise<object> {
+  //   return {
+  //     code: 200,
+  //     data: await this.articleService.findAll(),
+  //     message: 'Success',
+  //   };
+  // }
+  async findAll(@Query('search') name: string, @Query('pageNumber') pageNumber: number, @Query('pageSize') pageSize: number): Promise<object> {
+    try {
+      const res = await this.articleService.findAll(name, +pageNumber, +pageSize);
+
+      if (!res) {
+        throw new Error('Invalid page or size');
+      }
+
+      return res;
+    } catch (error) {
+      return {
+        code: 500,
+        message: error,
+      };
+    }
   }
+
   @Get(':id')
-  async findOne(@Param('id') id: number): Promise<object> {
-    let res = await this.articleService.findOne(id);
+  async findOne(@Param('id') id: string): Promise<object> {
+    let res = await this.articleService.findOne(+id);
     if (res[0]) {
       return {
         code: 200,
@@ -35,9 +53,9 @@ export class ArticleController {
   }
 
   @Delete(':id')
-  async deleteData(@Param('id') id: number): Promise<object> {
-    let res = await this.articleService.deleteData(id);
-    if (res.affected) {
+  async deleteData(@Param('id') id: string): Promise<object> {
+    let res = await this.articleService.deleteData(+id);
+    if (res) {
       return {
         code: 200,
         message: 'Success',
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Like, Repository } from 'typeorm';
 import { Article } from './article.entity';
 import { CreateArticleDTO, EditArticleDTO } from './article.dto';
 interface activeResponse<T = unknown> {
@@ -14,30 +14,52 @@ export class ArticleService {
   constructor(
     @InjectRepository(Article)
     private readonly articleModel: Repository<Article>
-  ) {}
+  ) { }
 
   // 查找所有数据
-  async findAll() {
-    return await this.articleModel.find();
+  // findAll() {
+  //   return this.articleModel.find();
+  // }
+  async findAll(search: string, pageNumber: number = 1, pageSize: number = 10) {
+    const totalSize = await this.articleModel.count()
+    // if (totalSize === 0 || pageNumber > Math.ceil(totalSize / pageSize)) {
+    //   return null;
+    // }
+
+    const res = await this.articleModel.findAndCount({
+      skip: (pageNumber - 1) * pageSize,
+      take: pageSize,
+      where: { title: Like(`%${search}%`) }
+    });
+
+    return {
+      data: res.length ? res[0] : [],
+      pageInfo: {
+        currentPage: pageNumber,
+        pageSize: pageSize,
+        totalSize: totalSize,
+        totalPages: Math.ceil(totalSize / pageSize),
+      },
+    };
   }
   //找一个
-  async findOne(id: number): Promise<CreateArticleDTO | object> {
-    return await this.articleModel.find({ where: { id: Number(id) } });
+  findOne(id: number): Promise<CreateArticleDTO | object> {
+    return this.articleModel.find({ where: { id: id } });
   }
   //创建数据
-  async createdArticle(body: CreateArticleDTO) {
+  createdArticle(body: CreateArticleDTO) {
     const res = this.articleModel.create({ ...body });
-    return await this.articleModel.save(res);
+    return this.articleModel.save(res);
   }
 
   //修改数据
-  async editArticle(body: EditArticleDTO){
-    const res = await this.articleModel.update({ id: Number(body.id) }, body);
+  editArticle(body: EditArticleDTO) {
+    const res = this.articleModel.update(+body.id, body);
     return res;
   }
-  
+
   //删除数据
-  async deleteData(id: number) {
-    return await this.articleModel.delete({ id: id });
+  deleteData(id: number) {
+    return this.articleModel.delete({ id: id });
   }
 }
